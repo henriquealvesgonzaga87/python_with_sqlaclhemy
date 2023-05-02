5ce70fed39685efe2f697ded2982f8ecdbc0e26c
Henrique <henriquealves87@hotmail.com>
2023-05-02 19:18:12 +0100

    this commit creates the queries on sqlite using sqlalchemy

--- project.py
+++ project.py
@@ -40,7 +40,7 @@ inspect_engine = inspect(engine)
 with Session(engine) as session:
 
     client1 = Client(
-        name="Client01",
+        name="client",
         tax_number="01010101",
         address="client01@mail.com",
         account=[Account(
@@ -50,6 +50,53 @@ with Session(engine) as session:
         )]
     )
 
-    session.add_all([client1])
+    client2 = Client(
+        name="client02",
+        tax_number="02020202",
+        address="client02@mail.com",
+        account=[Account(
+            agency_number="02",
+            account_number="0002",
+            bank_balance="20"
+        )]
+    )
+
+    session.add_all([client1, client2])
 
     session.commit()
+
+statment_client = select(Client).where(Client.name.in_(['client', 'client02']))
+print("Showing registered clients")
+for client in session.scalars(statment_client):
+    print(client)
+print()
+
+statment_account = select(Account).where(Account.id_client.in_([1, 2]))
+print("Showing registered accounts")
+for account in session.scalars(statment_account):
+    print(account)
+print()
+
+sorted_statement = select(Client).order_by(Client.id.desc())
+print("Showing the results in a sorted way")
+for result in session.scalars(sorted_statement):
+    print(result)
+print()
+
+join_statement = select(Client.name, Client.tax_number, Account.agency_number, Account.account_number,
+                        Account.bank_balance).join_from(Client, Account)
+for result in session.scalars(join_statement):
+    print(result)
+print()
+
+connection = engine.connect()
+results = connection.execute(join_statement).fetchall()
+print("executing the statment from the connection")
+for result in results:
+    print(result)
+print()
+
+count_statement = select(func.count('*')).select_from(Client)
+print("Showing the instances from Client")
+for result in session.scalars(count_statement):
+    print(result)
