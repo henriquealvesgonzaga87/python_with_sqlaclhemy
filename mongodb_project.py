88f5c29d22383c0ba202b118396498932ad6c3ad
Henrique <henriquealves87@hotmail.com>
2023-05-03 20:26:28 +0100

    this commit inserts data on db and show the results

--- mongodb_project.py
+++ mongodb_project.py
@@ -6,3 +6,31 @@ db_connection = pym.MongoClient(
 
 db = db_connection.test
 collection = db.test_collection
+
+data = [
+    {
+        "name": "jhon headhammer",
+        "tax_number": "010101",
+        "address": "headhammer@mail.com",
+        "account": {
+            "agency_number": "01",
+            "account_number": "0001",
+            "bank_balance": "1000.00"
+        }
+    },
+    {
+        "name": "sasha buttcracker",
+        "tax_number": "020202",
+        "address": "buttcracker@mail.com",
+        "account": {
+            "agency_number": "01",
+            "account_number": "0002",
+            "bank_balance": "2000.00"
+        }
+    }]
+
+inserting_data = db.posts
+result = inserting_data.insert_many(data)
+
+for post in inserting_data.find():
+    pprint.pprint(post)
